bf336b196f22ac62cba6bb6f3cd408f692f43564
Adam Gass <adam.gass@AdamGassMBP.local>
2022-10-31 15:25:34 -0400

    removed s3 backend hardcoded values

--- terraform.tf
+++ terraform.tf
@@ -5,11 +5,12 @@ terraform {
       version = "~> 4.0"
     }
   }
+  # set backend s3 configureation by adding the bucket name, region, and dynamo db table for locking
   backend "s3" {
-    bucket         = "vpc-tf-backend-363638675288"
+    bucket         = "<backend s3 bucket name>"
     key            = "backup-tf.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "tf-lock-table"
+    region         = "<AWS region>"
+    dynamodb_table = "<dynamo db table name>"
   }
 }
 
